3fb152cd815705262daf8b0bd5bd6122e2bee2a0
Neha <nehasijaria@gmail.com>
2022-01-06 00:00:21 -0500

    add About component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import './App.css';
 import Navbar from './component/Navbar';
 import TxtForm from './component/TxtForm';
+import AboutUs from './component/AboutUs';
 
 function App() {
   return (
     <>
       <Navbar title="MyTxtApp" />
-      
+      <div className="container">
         <TxtForm heading="Magical Box"/>
-      
+      {/* <AboutUs /> */}
+      </div>
     </>
   );
 }
