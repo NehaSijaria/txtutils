f6f4c38c8813cdde29f28cf029d49cf31bb459ed
Neha <nehasijaria@gmail.com>
2022-01-06 08:02:41 -0500

    enable toggle btn t display in nav

--- src/App.js
+++ src/App.js
@@ -5,11 +5,19 @@ import TxtForm from './component/TxtForm';
 import AboutUs from './component/AboutUs';
 
 function App() {
-  const [mode, setMode] = useState('dark') //chk if dark mode is on?
+  const [mode, setMode] = useState('light') //chk if dark mode is on?
 
+  const toggleMode = () => {
+    if(mode === 'light') {
+      setMode('dark')
+    }
+    else{
+      setMode('light')
+    }
+  }
   return (
     <>
-      <Navbar title="MyTxtApp" mode={mode}/>
+      <Navbar title="MyTxtApp" mode={mode} toggleMode={toggleMode}/>
       <div className="container">
         <TxtForm heading="Magical Box"/>
       {/* <AboutUs /> */}
