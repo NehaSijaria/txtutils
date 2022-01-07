61fd6492c12cce8647c855a97971aa6ee67b3449
Neha <nehasijaria@gmail.com>
2022-01-06 19:27:01 -0500

    add more col to change bkgroung

--- src/App.js
+++ src/App.js
@@ -19,10 +19,23 @@ function App() {
       },1500
     )
   }
-  const toggleMode = () => {
+  const removeBodyCls = () => {
+    document.body.classList.remove('bg-light')
+    document.body.classList.remove('bg-dark')
+    document.body.classList.remove('bg-warning')
+    document.body.classList.remove('bg-danger')
+    document.body.classList.remove('bg-success')
+  }
+
+  const toggleMode = (cls) => {
+    removeBodyCls()
+    document.body.classList.add('bg-' + cls)
     if(mode === 'light') {
       setMode('dark')
+        if (mode==='light') {
+      }
       document.body.style.backgroundColor='#042743'
+      
       showAlert('Enabled dark mode', 'success')
     }
     else{
